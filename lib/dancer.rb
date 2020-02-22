2c4fbd8edae48ab8cc91f312a95f7ed97b1b4d94
Hailey Bongo <haileyebongo@gmail.com>
2020-02-22 02:17:46 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -7,6 +7,12 @@ class Dancer
   include Dance 
   extend MetaDancing
   
+  extend FancyDance: :ClassMethods 
+  include
+  FancyDance: :InstanceMethods
+end
+  
+  
   attr_accessor :name 
   
   def initialize(name)
